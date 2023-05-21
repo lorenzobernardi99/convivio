77a90d5bf4cb3edf42d505e20873a9f667b7bfe2
lorenzobernardi99 <80358993+lorenzobernardi99@users.noreply.github.com>
2023-05-21 17:02:28 +0200

    Update send-order.js

--- assets/js/send-order.js
+++ assets/js/send-order.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Connect to MongoDB using Mongoose
-mongoose.connect('mongodb+srv://Lorenzo:Lorber031502@cluster0.vmlxnd9.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Jacopo:Mongoloide@atlascluster.ulvx08n.mongodb.net/Convivio.Orders', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
