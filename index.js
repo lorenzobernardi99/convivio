71fd156e72a56fe302cbfa4c8397d38c26026a25
Luca Landolfo <132918901+lucalanddd@users.noreply.github.com>
2023-05-24 10:06:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ db.connect();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('assets'));
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './index.html'));
