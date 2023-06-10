b287ef64b4c3533702ee0b06473690efc071251a
lorenzobernardi99 <80358993+lorenzobernardi99@users.noreply.github.com>
2023-06-10 02:08:52 +0200

    Add files via upload

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 const dbUrl = "mongodb+srv://Jacopo:Mongoloide@atlascluster.ulvx08n.mongodb.net/?retryWrites=true&w=majority"
-//"mongodb://localhost/user";
 
 const connect = async () => {
   mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
